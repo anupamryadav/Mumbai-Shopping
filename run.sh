42c2c82e0627fd9643d9fef9d24a592d799941a8
Anupamryadav <anupamryadav@gmail.com>
2018-10-02 20:16:03 +0530

    Auto-generated commit

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-PATH=$PATH:$(npm bin)
-set -x
-# Production build
-ng build --prod
-# Serve
-cd dist/Mumbai-Shopping
-http-server
--- src/environments/firebaseConfig.ts
+++
@@ -1,8 +0,0 @@
-export const FireBaseConfig = {
-  apiKey: "AIzaSyBkES1o7q82qy2I8eaMHl24u4VZf3VAGSM",
-    authDomain: "optical-hangar-88010.firebaseapp.com",
-    databaseURL: "https://optical-hangar-88010.firebaseio.com",
-    projectId: "optical-hangar-88010",
-    storageBucket: "optical-hangar-88010.appspot.com",
-    messagingSenderId: "851359413193"
-};
